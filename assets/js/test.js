ca71862d83db15354a7f7be239a9137b659c277c
Matt Titmuss <45769440+mattmarc44@users.noreply.github.com>
2019-04-08 17:33:33 +0100

    select menu getting correct data

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,6 +1,6 @@
 const baseUrl = 'https://statsapi.web.nhl.com//api/v1/teams?expand=team.stats';
 
-function getData(type, cb) {
+function getData(cb) {
     //create request
     let xhr = new XMLHttpRequest;
 
@@ -16,15 +16,6 @@ function getData(type, cb) {
     };
 }
 
-function writeToDocument(type) {
-    getData(type, function (data) {
-        data = data.teams[0].teamStats[0].splits[0].stat; //targetdata = teamStats[0].splits[0].stat;
-            //console.log(data.faceOffsWon);
-            document.getElementById('data').innerHTML = data[type];
-    });
-}
-
-
 /*
 THEORY
 
@@ -33,3 +24,12 @@ use a variable to store the path to teamstats.
 attach targeted stat to the end to access that data.
 sommehow make that a chart.
 */
+function writeToDocument() {
+    getData(function (data) {
+        var teamNo = document.getElementById('teams').value;
+        var statType = document.getElementById('stat-select').value;
+        data = data.teams[teamNo].teamStats[0].splits[0].stat[statType]; //var targetdata = teamStats[0].splits[0].stat;
+            //console.log(teamNo);
+            document.getElementById('data').innerHTML = data; //`<p>${data[type]}</p> + <p>${data[typeTwo]}</p>`
+    });
+}
